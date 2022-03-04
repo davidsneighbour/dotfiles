9a6f0f30861d80026fead1f4c9bdbed009092bfb
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-03-04 17:12:25 +0700

    theme: update themer configuration

--- themer/colors.js
+++ themer/colors.js
@@ -2,17 +2,41 @@
 
 module.exports.colors = {
   "dark": {
-    "shade0": "#23292D",
-    "shade7": "#FDFDFD",
-    "accent0": "#EF4E7C",
-    "accent1": "#F37055",
-    "accent2": "#F79532",
-    "accent3": "#6EBB82",
-    "accent4": "#09B399",
-    "accent5": "#1299AD",
-    "accent6": "#5073B8",
-    "accent7": "#A067AB"
+    "shade0": "#002B36",
+    "shade1": "#073642",
+    "shade2": "#586E75",
+    "shade3": "#657B83",
+    "shade4": "#839496",
+    "shade5": "#93A1A1",
+    "shade6": "#EEE8D5",
+    "shade7": "#FDF6E3",
+    "accent0": "#DC322F",
+    "accent1": "#CB4B16",
+    "accent2": "#B58900",
+    "accent3": "#859900",
+    "accent4": "#2AA198",
+    "accent5": "#268BD2",
+    "accent6": "#6C71C4",
+    "accent7": "#D33682"
+  },
+  "light": {
+    "shade0": "#FDF6E3",
+    "shade1": "#EEE8D5",
+    "shade2": "#93A1A1",
+    "shade3": "#839496",
+    "shade4": "#657B83",
+    "shade5": "#586E75",
+    "shade6": "#073642",
+    "shade7": "#002B36",
+    "accent0": "#DC322F",
+    "accent1": "#CB4B16",
+    "accent2": "#B58900",
+    "accent3": "#859900",
+    "accent4": "#2AA198",
+    "accent5": "#268BD2",
+    "accent6": "#6C71C4",
+    "accent7": "#D33682"
   }
 };
 
-// Your theme's URL: https://themer.dev/?colors.dark.accent0=%23EF4E7C&colors.dark.accent1=%23F37055&colors.dark.accent2=%23F79532&colors.dark.accent3=%236EBB82&colors.dark.accent4=%2309B399&colors.dark.accent5=%231299AD&colors.dark.accent6=%235073B8&colors.dark.accent7=%23A067AB&colors.dark.shade0=%2323292D&colors.dark.shade7=%23FDFDFD&activeColorSet=dark&calculateIntermediaryShades.dark=true&calculateIntermediaryShades.light=true
+// Your theme's URL: https://themer.dev/?colors.dark.accent0=%23DC322F&colors.dark.accent1=%23CB4B16&colors.dark.accent2=%23B58900&colors.dark.accent3=%23859900&colors.dark.accent4=%232AA198&colors.dark.accent5=%23268BD2&colors.dark.accent6=%236C71C4&colors.dark.accent7=%23D33682&colors.dark.shade0=%23002B36&colors.dark.shade1=%23073642&colors.dark.shade2=%23586E75&colors.dark.shade3=%23657B83&colors.dark.shade4=%23839496&colors.dark.shade5=%2393A1A1&colors.dark.shade6=%23EEE8D5&colors.dark.shade7=%23FDF6E3&colors.light.accent0=%23DC322F&colors.light.accent1=%23CB4B16&colors.light.accent2=%23B58900&colors.light.accent3=%23859900&colors.light.accent4=%232AA198&colors.light.accent5=%23268BD2&colors.light.accent6=%236C71C4&colors.light.accent7=%23D33682&colors.light.shade0=%23FDF6E3&colors.light.shade1=%23EEE8D5&colors.light.shade2=%2393A1A1&colors.light.shade3=%23839496&colors.light.shade4=%23657B83&colors.light.shade5=%23586E75&colors.light.shade6=%23073642&colors.light.shade7=%23002B36&activeColorSet=dark&calculateIntermediaryShades.dark=false&calculateIntermediaryShades.light=false
