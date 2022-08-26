6c630e8bd8db0f92d986770ba97f512c05c76696
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-08-26 15:48:03 +0700

    chore: some unorganised changes

--- themer/colors.js
+++ themer/colors.js
@@ -1,5 +1,3 @@
-// This file can be used with the themer CLI, see https://github.com/themerdev/themer
-
 module.exports.colors = {
   "dark": {
     "shade0": "#002B36",
@@ -38,5 +36,3 @@ module.exports.colors = {
     "accent7": "#D33682"
   }
 };
-
-// Your theme's URL: https://themer.dev/?colors.dark.accent0=%23DC322F&colors.dark.accent1=%23CB4B16&colors.dark.accent2=%23B58900&colors.dark.accent3=%23859900&colors.dark.accent4=%232AA198&colors.dark.accent5=%23268BD2&colors.dark.accent6=%236C71C4&colors.dark.accent7=%23D33682&colors.dark.shade0=%23002B36&colors.dark.shade1=%23073642&colors.dark.shade2=%23586E75&colors.dark.shade3=%23657B83&colors.dark.shade4=%23839496&colors.dark.shade5=%2393A1A1&colors.dark.shade6=%23EEE8D5&colors.dark.shade7=%23FDF6E3&colors.light.accent0=%23DC322F&colors.light.accent1=%23CB4B16&colors.light.accent2=%23B58900&colors.light.accent3=%23859900&colors.light.accent4=%232AA198&colors.light.accent5=%23268BD2&colors.light.accent6=%236C71C4&colors.light.accent7=%23D33682&colors.light.shade0=%23FDF6E3&colors.light.shade1=%23EEE8D5&colors.light.shade2=%2393A1A1&colors.light.shade3=%23839496&colors.light.shade4=%23657B83&colors.light.shade5=%23586E75&colors.light.shade6=%23073642&colors.light.shade7=%23002B36&activeColorSet=dark&calculateIntermediaryShades.dark=false&calculateIntermediaryShades.light=false
---
+++ themer/theme.js
@@ -0,0 +1,32 @@
+accent0:
+#DC322F
+
+error, vcs deletion, ANSI red
+accent1:
+#CB4B16
+
+syntax
+accent2:
+#B58900
+
+warning, vcs modification, ANSI yellow
+accent3:
+#859900
+
+success, vcs addition, ANSI green
+accent4:
+#2AA198
+
+syntax, ANSI cyan
+accent5:
+#268BD2
+
+syntax, ANSI blue
+accent6:
+#6C71C4
+
+syntax, caret / cursor
+accent7:
+#D33682
+
+syntax, special, ANSI magenta
