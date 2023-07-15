f99eede637c5094a99dc01486f2a7ab8f2a78420
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-07-15 15:23:53 +0700

    fix: proper npm setup for cronjob

--- bin/cronjobs/update-npm.sh
+++ bin/cronjobs/update-npm.sh
@@ -5,6 +5,21 @@ echo "starting update-npm.sh"
 # exit if any command fails
 set -e
 
+source /home/patrick/.bashrc
+
+
+# Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 20.3.0
+
+# now node works
+node -e "console.log('hello')"
+node --version
+
+# npm works too!
+npm --version
+
 # update global npm packages
 /home/patrick/.nvm/versions/node/v20.3.0/bin/npm -g install svgo cypress fixpack jshint \
     lerna-wizard lerna lighthouse netlify-cli \
