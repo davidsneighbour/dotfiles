b264d0d7acdab38120c3d66333c9c41881d6ae05
Patrick Kollitsch <patrick@davids-neighbour.com>
2023-09-15 20:00:08 +0700

    fix: update all available nvm versions

--- bin/cronjobs/update-npm.sh
+++ bin/cronjobs/update-npm.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
+echo "##########################################################################"
 echo "starting update-npm.sh"
+echo "##########################################################################"
 
 # exit if any command fails
 set -e
 
-source /home/patrick/.bashrc
-
-
 # Load nvm
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm use 20.3.0
+# nvm use
 
-# now node works
-node -e "console.log('hello')"
-node --version
+for DIRNAME in /home/patrick/.nvm/versions/node/*/; do
 
-# npm works too!
-npm --version
+    #   if [[ $DIR == 'references' ]]; then
+    #     continue
+    #   fi
+    DIR=$(basename "$DIRNAME")
+    nvm use $DIR
+    # update global npm packages
+    npm -g install svgo cypress fixpack jshint \
+        lerna-wizard lerna lighthouse netlify-cli \
+        npm-check-updates svgo typescript \
+        @davidsneighbour/remark-config \
+        @socketsecurity/cli
 
-# update global npm packages
-/home/patrick/.nvm/versions/node/v20.3.0/bin/npm -g install svgo cypress fixpack jshint \
-    lerna-wizard lerna lighthouse netlify-cli \
-    npm-check-updates svgo typescript \
-    @davidsneighbour/remark-config \
-    @socketsecurity/cli
+done
 
+echo "##########################################################################"
 echo "done with update-npm.sh"
+echo "##########################################################################"
+
+npm use
