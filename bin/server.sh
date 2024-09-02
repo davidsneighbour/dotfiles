0db98709ee9b20448ed5ba2d8fee795a33fca2e8
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-09-02 11:40:41 +0700

    wip: set up system
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

---
+++ bin/server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run -d \
+  -p 3000:8080 \
+  -v ollama:/root/.ollama \
+  -v open-webui:/home/patrick/github.com/davidsneighbour/alan/brain --name open-webui \
+  --restart always \
+  \
+  -e WEBUI_AUTH=false \
+  -e WEBUI_AUTH="Alan" \
+  -e ENABLE_COMMUNITY_SHARING=false \
+  \
+  ghcr.io/open-webui/open-webui:ollama
