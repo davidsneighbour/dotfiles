d554fa21bf49ffa44df8d5d79135058d2cdadde8
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-11-15 09:47:18 +0700

    setup(vim): add dracula theme to vim setup

--- setup/install/080-dracula-theme-setup.sh
+++ setup/install/080-dracula-theme-setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+echo "Setup Dracula theme..."
+
 # vscode
+echo "Setup Dracula theme on VSCode..."
 mkdir -p ~/.vscode/extensions
-git clone https://github.com/dracula/visual-studio-code.git ~/.vscode/extensions/theme-dracula
+git clone https://github.com/dracula/visual-studio-code.git ~/.vscode/extensions/theme-dracula 2>/dev/null || true
 cd ~/.vscode/extensions/theme-dracula || exit
-npm install
-npm run build
+npm install --silent
+npm run build --silent
 # todo: remove plugin from profiles if existing
 
 # vim 8.2+
+echo "Setup Dracula theme on Vim..."
 mkdir -p ~/.vim/pack/themes/start
-cd ~/.vim/pack/themes/start || exit
-git clone https://github.com/dracula/vim.git dracula
+git clone https://github.com/dracula/vim.git ~/.vim/pack/themes/start/dracula 2>/dev/null || true
