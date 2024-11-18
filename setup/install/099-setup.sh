85ba41216b38236b52296858c22e2c9f9b6abea7
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-11-18 18:32:16 +0700

    feat: set wallpaper

--- setup/install/099-setup.sh
+++ setup/install/099-setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Disable automatically mounting external hard drives
 gsettings set org.gnome.desktop.media-handling automount false
 gsettings set org.gnome.desktop.media-handling automount-open false
+
+# Set the wallpaper
+gsettings set org.gnome.desktop.background picture-uri "file://${HOME}/.wallpapers/wallpaper001.jpg"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://${HOME}/.wallpapers/wallpaper001.jpg"
