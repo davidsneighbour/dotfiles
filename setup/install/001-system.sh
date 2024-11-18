ffd5ce3578b63d2456e983a43fc55766447e0fc4
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-11-18 18:32:16 +0700

    feat: set wallpaper

--- setup/install/001-system.sh
+++ setup/install/001-system.sh
@@ -2,14 +2,14 @@
 
 sudo apt update
 sudo apt remove --purge totem libreoffice sssd vlc -y
-sudo apt install apt-transport-https arp-scan barrier bsdmainutils \
+sudo apt install ack apt-transport-https arp-scan barrier bsdmainutils kitty \
   build-essential ca-certificates chrome-gnome-shell curl dconf-cli flameshot \
   gimp git gnome-shell-extension-manager gnome-terminal gnome-tweaks gnupg2 \
-  gparted gpg grc inkscape libavcodec-dev libavcodec-extra \
+  gparted gpg grc inkscape libavcodec-dev libavcodec-extra dconf-editor \
   libayatana-appindicator3-dev libfuse2 libnss3-tools net-tools pdftk plocate \
   polybar python-is-python3 python3 python3-gpg python3-pip python3-virtualenv \
-  qt5ct rename shutter shc smartctl software-properties-common rofi \
-  texlive-extra-utils testdisk ubuntu-restricted-extras vim wget zoxide -y
+  qt5ct rename shutter shc smartmontools software-properties-common rofi \
+  texlive-extra-utils testdisk ubuntu-restricted-extras vim wget zoxide fzf w3m w3m-img chafa -y
 sudo apt upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean -y
