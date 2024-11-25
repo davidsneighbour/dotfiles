3a87c6e756a6616c4b841084f16d23a0a8fa2e4a
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2024-11-25 16:14:07 +0700

    fix: updater and local config volume
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- bin/server.sh
+++ bin/server.sh
@@ -3,7 +3,9 @@
 docker run -d \
   -p 3000:8080 \
   -v ollama:/root/.ollama \
-  -v open-webui:/home/patrick/github.com/davidsneighbour/alan/brain --name open-webui \
+  --add-host=host.docker.internal:host-gateway \
+  -v /home/patrick/github.com/davidsneighbour/alan/brain:/app/backend/data \
+  --name open-webui \
   --restart always \
   \
   -e WEBUI_AUTH=false \
