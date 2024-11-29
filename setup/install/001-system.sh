4220aa69c3e96dcfbbc9b3c13c9e1026376bc197
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-11-29 07:58:12 +0700

    fix: update setup scripts

--- setup/install/001-system.sh
+++ setup/install/001-system.sh
@@ -6,10 +6,11 @@ sudo apt install ack apt-transport-https arp-scan barrier bsdmainutils kitty \
   build-essential ca-certificates chrome-gnome-shell curl dconf-cli flameshot \
   gimp git gnome-shell-extension-manager gnome-terminal gnome-tweaks gnupg2 \
   gparted gpg grc inkscape libavcodec-dev libavcodec-extra dconf-editor \
-  libayatana-appindicator3-dev libfuse2 libnss3-tools net-tools pdftk plocate \
-  polybar python-is-python3 python3 python3-gpg python3-pip python3-virtualenv \
-  qt5ct rename shutter shc smartmontools software-properties-common rofi \
-  texlive-extra-utils testdisk ubuntu-restricted-extras vim wget zoxide fzf w3m w3m-img chafa -y
+  libayatana-appindicator3-dev libfuse2 fuse3 libnss3-tools net-tools pdftk \
+  plocate polybar python-is-python3 python3 python3-gpg python3-pip \
+  python3-virtualenv qt5ct rename shutter shc smartmontools \
+  software-properties-common rofi texlive-extra-utils testdisk \
+  ubuntu-restricted-extras vim wget zoxide fzf w3m w3m-img chafa -y
 sudo apt upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean -y
