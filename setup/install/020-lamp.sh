564b2c52d6e25e15908fb5f415e0d43aded3d32c
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-11-29 08:46:27 +0700

    wip: lamp setup

--- setup/install/020-lamp.sh
+++ setup/install/020-lamp.sh
@@ -6,18 +6,9 @@ sudo a2disconf php8*
 reload
 install_php_packages
 sudo chown patrick:patrick /var/log/boot.log
-sudo service apache2 status
 sudo systemctl -f apache2 remove
 sudo systemctl disable apache2
 sudo systemctl disable mysql
 sudo service apache2 start && sudo service mysql start
 sudo service mysql stop && sudo service apache2 stop
 sudo service apache2 status
-sudo systemctl -f apache2 remove
-sudo systemctl disable apache2
-sudo systemctl disable mysql
-sudo service apache2 start && sudo service mysql start
-sudo service mysql stop && sudo service apache2 stop
-sudo service apache2 status
-
-
