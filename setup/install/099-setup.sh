2cbc9efe2d22f861092ef45dfa580b096927bbac
Patrick Kollitsch <patrick@davids-neighbour.com>
2025-01-16 18:27:21 +0700

    fix: update setup configurations

--- setup/install/099-setup.sh
+++ setup/install/099-setup.sh
@@ -7,3 +7,9 @@ gsettings set org.gnome.desktop.media-handling automount-open false
 # Set the wallpaper
 gsettings set org.gnome.desktop.background picture-uri "file://${HOME}/.wallpapers/wallpaper001.jpg"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://${HOME}/.wallpapers/wallpaper001.jpg"
+
+# Set temperature unit to centigrade
+gsettings set org.gnome.GWeather4 temperature-unit 'centigrade'
+
+# Set the order of apps to alphabetical
+gsettings set org.gnome.shell app-picker-layout "[]"
