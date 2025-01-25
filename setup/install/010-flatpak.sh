0e69e969b05b03928a76cf3c4dc4be07b10476cd
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-01-25 17:39:32 +0700

    fix: update setup scripts

--- setup/install/010-flatpak.sh
+++ setup/install/010-flatpak.sh
@@ -8,3 +8,4 @@ sudo apt install flatpak gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.gnome.Extensions
 flatpak install flathub ca.desrt.dconf-editor
+flatpak install flathub io.github.flattool.Ignition
--- setup/install/020-node.sh
+++ setup/install/020-node.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 nvm install --lts
 nvm install v20
 nvm install v22
+nvm install v23
 nvm use --lts
 npm login
-
-# quick hack because npm 10.8.0 freezes
-npm install -g npm@10.3.0
