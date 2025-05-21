dd9ba28e4f78fa90169781f099f1ffab6397e434
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-05-22 06:25:33 +0700

    build(fix): release script fixes

--- .versionrc.cjs
+++ .versionrc.cjs
@@ -1,5 +1,8 @@
 const defaultStandardVersion = require('@davidsneighbour/release-config');
 const localStandardVersion = {
+  scripts: {
+    prerelease: "./bashrc/helpers/prerelease",
+  },
   bumpFiles: [
     {
       filename: "package.json",
