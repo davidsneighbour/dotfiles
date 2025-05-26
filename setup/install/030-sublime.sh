c5ef66d52f418502ddccb873f41aa8caed27d120
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-05-27 06:45:48 +0700

    fix(setup): update sublime edit and merge setups

--- setup/install/030-sublime.sh
+++ setup/install/030-sublime.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | \
-  gpg --dearmor | \
-  sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | \
-  sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
 sudo apt update && sudo apt install sublime-text sublime-merge
