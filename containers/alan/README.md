0b0b9cc361a15d5029d102c9106dac8edefdc65e
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-07-31 07:24:35 +0700

    chore(git): add submodules and cleanup

--- containers/alan/README.md
+++
@@ -1,3 +0,0 @@
-# Setup
-
-- Install Docker ([Linux instructions](https://docs.docker.com/desktop/install/linux-install/))
--- containers/alan/update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm --volume /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --run-once open-webui
