7e5d9a254b80fcc9b9fcc71a8fb387eb6e2f4e69
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2025-12-11 04:34:28 +0700

    fix: add ignored folders to synch cronjob

--- bin/cronjobs/synch_downloads.sh
+++ bin/cronjobs/synch_downloads.sh
@@ -22,12 +22,16 @@ mkdir -p "${LOG_DIR}"
     patrick@192.168.1.201:/home/patrick/Downloads/ \
     /home/patrick/Downloads/000_unkle \
     --exclude '.incoming' \
+    --exclude '*.crdownload' \
+    --exclude 'Takeout/' \
+    --exclude 'takeout-*' \
     --delete
 
   rsync -rztv \
     /home/patrick/Downloads/000_synchback \
     patrick@192.168.1.201:/home/patrick/Downloads/ \
     --exclude '.incoming' \
+    --exclude '*.crdownload' \
     --delete
 
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] download-synch backup done"
