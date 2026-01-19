b7836038c6b4224b11f6bbe22709a7a63863d767
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2026-01-20 04:34:00 +0700

    config(dotbot): add conky setup to dotbot

---
+++ configs/system/conky/desktop/datetime.lua
@@ -0,0 +1,51 @@
+conky.config = {
+  --[[
+    Shows a watch and date on the desktop.
+  ]]
+	use_xft = true,
+	font = '118:size=8',
+	xftalpha = 0.1,
+	update_interval = 1,
+	total_run_times = 0,
+
+	own_window = true,
+	own_window_type = 'normal',
+	own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
+  -- for transparency
+	own_window_transparent = true,
+	own_window_argb_visual = true,
+	own_window_argb_value = 144,
+
+	double_buffer = true,
+--minimum_size 250 5
+--maximum_width 500
+	draw_shades = false,
+	draw_outline = false,
+	draw_borders = false,
+	draw_graph_borders = false,
+	default_color = 'white',
+	default_shade_color = 'red',
+	default_outline_color = 'green',
+	alignment = 'top_right',
+	gap_x = 200,
+	gap_y = 30,
+	no_buffers = true,
+	uppercase = false,
+	cpu_avg_samples = 2,
+	net_avg_samples = 1,
+	override_utf8_locale = true,
+	use_spacer = 'left',
+
+
+	minimum_width = 0, minimum_height = 0,
+};
+
+conky.text = [[
+${voffset 10}${color EAEAEA}${font GE Inspira:pixelsize=120}${time %I:%M}${font}${voffset -84}${offset 10}${color FFA300}${font GE Inspira:pixelsize=42}${time %d} ${voffset -15}${color EAEAEA}${font GE Inspira:pixelsize=22}${time  %B} ${time %Y}${font}${voffset 24}${font GE Inspira:pixelsize=58}${offset -148}${time %A}${font}
+${voffset 1}${offset 12}${font Ubuntu:pixelsize=12}${color FFA300}HD ${offset 9}$color${fs_free /} / ${fs_size /}${offset 30}${color FFA300}RAM ${offset 9}$color$mem / $memmax${offset 30}${color FFA300}CPU ${offset 9}$color${cpu cpu0}%
+
+
+
+
+# empty lines for spacing
+]];
