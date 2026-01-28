7aa0c1a3bc4174a3ea98d4ea123e1ecaf7db03ad
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-01-29 05:27:55 +0700

    feat(conky): update conky configuration for date/time section

--- configs/system/conky/desktop/datetime.lua
+++ configs/system/conky/desktop/datetime.lua
@@ -1,53 +1,55 @@
+
+-- https://conky.cc/config_settings
 conky.config = {
   --[[
     Shows a watch and date on the desktop.
   ]]
-	use_xft = true,
-	font = '118:size=8',
-	xftalpha = 0.1,
-	update_interval = 1,
-	total_run_times = 0,
 
+  background = true, -- fork to the background, NOT colour
+  color1 = 'ff79c6',
   -- window specifications
 	own_window = true,
 	own_window_type = 'normal',
 	own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
-
   -- for transparency
 	own_window_transparent = true,
 	own_window_argb_visual = true,
 	own_window_argb_value = 144,
-
-	double_buffer = true,
---minimum_size 250 5
---maximum_width 500
-	draw_shades = false,
-	draw_outline = false,
-	draw_borders = false,
-	draw_graph_borders = false,
-	default_color = 'white',
-	default_shade_color = 'red',
-	default_outline_color = 'green',
-	alignment = 'top_right',
+  -- positioning
+  alignment = 'top_right',
 	gap_x = 30,
 	gap_y = 30,
-	no_buffers = true,
-	uppercase = false,
-	cpu_avg_samples = 2,
-	net_avg_samples = 1,
-	override_utf8_locale = true,
-	use_spacer = 'left',
-	minimum_width = 0, minimum_height = 0,
+  -- helpers
+	draw_shades = false,
+	-- default_shade_color = 'red',
+	draw_outline = false,
+	-- default_outline_color = 'green',
+	draw_borders = false, -- set to true to debug container
+	draw_graph_borders = false,
+  border_width = 1,
+  -- output formatting
+	font = 'Ubuntu:pixelsize=24',
+	use_xft = true,
+  short_units = true,
+	update_interval = 10,
+  use_spacer = 'left',
+
+	xftalpha = 0.1,
+	total_run_times = 0,
+
 };
 
-conky.text = [[
-${voffset 10}
-${alignr}${color EAEAEA}${font GE Inspira:pixelsize=130}${time %I:%M}${font}
-${alignr}${voffset -5}${color FFA300}${font GE Inspira:pixelsize=42}${time %d %B %Y}${font}${color}
-${alignr}${voffset -10}${font GE Inspira:pixelsize=58}${time %A}${font}
+-- https://conky.cc/variables
 
-${alignr}${font GE Inspira:pixelsize=24}${color FFA300}HD1 ${offset 9}$color${fs_free /} / ${fs_size /}${font}
-${alignr}${font GE Inspira:pixelsize=24}${color FFA300}HD2 ${offset 9}$color${fs_free /home} / ${fs_size /home}${font}
-${alignr}${font GE Inspira:pixelsize=24}${color FFA300}RAM ${offset 9}$color$mem / $memmax${font}
-${alignr}${font GE Inspira:pixelsize=24}${color FFA300}CPU ${offset 9}$color${cpu cpu0}%${font}
+conky.text = [[
+${alignr}${color EAEAEA}${font Ubuntu:pixelsize=130}${time %I:%M}${font}
+${alignr}${voffset -5}${color1}${font Ubuntu:pixelsize=42}${time %d %B %Y}${font}${color}
+${alignr}${voffset -10}${font Ubuntu:pixelsize=58}${time %A}${font}
+
+${alignr}${color1}HD1  $color${fs_free /} / ${fs_size /}${font}
+${alignr}${color1}HD2  $color${fs_free /home} / ${fs_size /home}${font}
+${alignr}${color1}RAM  $color$mem / $memmax${font}
+${alignr}${color1}CPU  $color${cpu cpu0}%${font}
+${alignr}${color1}Uptime  $color${uptime}${font}
+
 ]];
