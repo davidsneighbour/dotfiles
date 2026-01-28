21bc0c0113514153b72b18236f6bd7dbf320b664
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2026-01-29 05:27:55 +0700

    feat(conky): update conky configuration for date/time section

--- configs/system/conky/startup.sh
+++ configs/system/conky/startup.sh
@@ -4,6 +4,6 @@ if [ "$DESKTOP_SESSION" = "ubuntu" ]; then
    sleep 20s
    killall conky
    cd "$HOME/.config/conky/desktop/"
-   conky -c "$HOME/.config/conky/desktop/datetime.lua" &
+   conky -q -c "$HOME/.config/conky/desktop/datetime.lua" &
    exit 0
 fi
