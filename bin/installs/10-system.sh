d2a3ebb518c4abc5ad5872745227a558b2baec46
Patrick Kollitsch <davidsneighbourdev@gmail.com>
2026-02-15 06:55:43 +0700

    feat: add apt install set

---
+++ bin/installs/10-system.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install -y \
+    build-essential \
+    curl \
+    git \
+    git-delta \
+    htop \
+    neofetch \
+    software-properties-common \
+    unzip \
+    wget
