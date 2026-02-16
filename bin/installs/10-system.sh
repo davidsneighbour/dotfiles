1461bf9516b032b639391bef3372909d125e285b
Patrick Kollitsch <davidsneighbourdev@gmail.com>
2026-02-17 06:28:18 +0700

    fix: moving papirus into apt setup

--- bin/installs/10-system.sh
+++ bin/installs/10-system.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:libreoffice/ppa --yes --no-update
+sudo add-apt-repository ppa:papirus/papirus --yes --no-update
+
+sudo apt update
+sudo apt upgrade --yes
+sudo apt dist-upgrade --yes
+sudo apt autoremove --yes
+sudo apt clean --yes
+
 sudo apt install -y \
     build-essential \
     curl \
-    git \
-    git-delta \
+    filezilla filezilla-theme-papirus filezilla-common \
+    git  git-delta git-extras \
     htop \
+    papirus-icon-theme papirus-folders papirus-colors \
     qbittorrent \
     software-properties-common \
     unzip \
+    vlc \
     wget \
     yamllint
