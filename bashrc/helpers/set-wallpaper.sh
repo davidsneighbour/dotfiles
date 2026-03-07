ac6ab66358a1749390d2718d8e02c4d60bbb296f
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-03-08 06:43:19 +0700

    feat(desktop): add wallpaper switcher script (based on wm)
    - add a wallpaper switcher that can handle all default wallpaper handlers
    
    BREAKING CHANGE: wall paper handling

---
+++ bashrc/helpers/set-wallpaper.sh
@@ -0,0 +1,1 @@
+../../configs/wallpapers/set-wallpaper.sh
--- bashrc/installs/10-system.sh
+++ bashrc/installs/10-system.sh
@@ -14,6 +14,7 @@ sudo apt install -y \
   conky-all apcupsd audacious moc mpd \
   coreutils \
   curl \
+  feh \
   ffmpeg \
   filezilla filezilla-theme-papirus filezilla-common \
   git git-delta git-extras \
