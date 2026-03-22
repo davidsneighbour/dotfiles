afb491505971d084cd3c266cf5333fc0d11ab202
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-03-22 21:38:04 +0700

    fix: apply proper nvm initialisation on terminal start

--- bashrc/partials/_programs/110-nvm.sh
+++ bashrc/partials/_programs/110-nvm.sh
@@ -5,4 +5,9 @@ export NVM_DIR="${HOME}/.nvm"
 
 if dnb_is_interactive; then
   [ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"
+
+  # Ensure default Node version has its global npm binaries/completions available
+  if command -v nvm >/dev/null 2>&1; then
+    nvm use --silent default >/dev/null 2>&1
+  fi
 fi
