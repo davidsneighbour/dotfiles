13e0124a465f484cd2c8cacb9284d590938b7c80
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-04-02 12:21:44 +0700

    feat: add bootstrap.sh to set up a couple of folders and symlinks

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -s "${HOME}/github.com/davidsneighbour/dotfiles/modules/dotfiles-ai" "${HOME}/github.com/davidsneighbour/dotfiles-ai"
+ln -s "${HOME}/github.com/davidsneighbour/dotfiles/modules/gmailctl" "${HOME}/github.com/davidsneighbour/gmailctl-config"
+ln -s "${HOME}/github.com/davidsneighbour/dotfiles/modules/protected" "${HOME}/github.com/davidsneighbour/dotfiles-protected"
