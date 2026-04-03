d079ef98089b5a0edb0d91e8d2b6592c530cc4f4
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-04-04 05:32:30 +0700

    fix: update autostart setup for locutus

--- configs/system/autostart/locutus/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$DESKTOP_SESSION" = "xubuntu" ]; then
-  sleep 20s
-  killall conky
-  cd "$HOME/.config/conky/desktop/"
-  conky -c "$HOME/.config/conky/desktop/datetime.lua" &
-  exit 0
-fi
