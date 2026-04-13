d79ed823482f4125b0f091bb1ebb381361c84c29
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-04-13 10:14:06 +0700

    fix(conky): formatting changes for datetime widget

--- configs/system/conky/desktop/datetime.lua
+++ configs/system/conky/desktop/datetime.lua
@@ -1,38 +1,38 @@
 
 -- https://conky.cc/config_settings
 conky.config = {
-  --[[
+    --[[
     Shows a watch and date on the desktop.
-  ]]
+    ]]
 
-  background = true, -- fork to the background, NOT colour
-  color1 = 'ff79c6',
-  -- window specifications
+    background = true, -- fork to the background, NOT colour
+    color1 = 'ff79c6',
+    -- window specifications
 	own_window = true,
 	own_window_type = 'normal',
 	own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
-  -- for transparency
+    -- for transparency
 	own_window_transparent = true,
 	own_window_argb_visual = true,
 	own_window_argb_value = 144,
-  -- positioning
-  alignment = 'bottom_right',
+    -- positioning
+    alignment = 'bottom_right',
 	gap_x = 30,
 	gap_y = 30,
-  -- helpers
+    -- helpers
 	draw_shades = false,
 	-- default_shade_color = 'red',
 	draw_outline = false,
 	-- default_outline_color = 'green',
 	draw_borders = false, -- set to true to debug container
 	draw_graph_borders = false,
-  border_width = 1,
-  -- output formatting
-	font = 'Ubuntu:pixelsize=24',
+    border_width = 1,
+    -- output formatting
+	font = 'Monaspace Argon:pixelsize=24',
 	use_xft = true,
-  short_units = true,
+    short_units = true,
 	update_interval = 10,
-  use_spacer = 'left',
+    use_spacer = 'left',
 
 	xftalpha = 0.1,
 	total_run_times = 0,
@@ -42,7 +42,7 @@ conky.config = {
 -- https://conky.cc/variables
 
 conky.text = [[
-${alignc}${color EAEAEA}${font Ubuntu:pixelsize=180}${time %I:%M}${font}
-${alignc}${voffset -5}${color1}${font Ubuntu:pixelsize=58}${time %d %B %Y}${font}${color}
-${alignc}${voffset -10}${font Ubuntu:pixelsize=74}${time %A}${font}
+${alignc}${color EAEAEA}${font Bahn Pro:pixelsize=180}${time %I:%M}${font}
+${alignc}${color1}${font Bahn Pro:pixelsize=58}${time %d %B %Y}${font}${color}
+${alignc}${font Bahn Pro:pixelsize=150}${exec date +"%A" | tr '[:lower:]' '[:upper:]'}${font}
 ]];
