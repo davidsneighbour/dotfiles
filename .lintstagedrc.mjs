5ee3cfb04a8855546b56c933f85ad6352ac317d6
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-04-27 14:58:20 +0700

    ci: more linting on staging

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -5,7 +5,7 @@
  * @type {import("lint-staged").Configuration}
  */
 export default {
-  // "*.{json,jsonc}": ["biome check --staged"],
+  "*.{json,jsonc}": ["biome check --write --staged"],
   // ".github/workflows/**/*.y(a?)ml": [
   //   "zizmor --no-exit-codes",
   // ],
@@ -13,25 +13,15 @@ export default {
     "lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm",
   ],
   "*.{ts,tsx,(m|c)js,jsx}": (/** @type {string[]} */ files) => {
-    return [`biome check --no-errors-on-unmatched ${files.join(" ")}`];
+    return [`biome check --write --no-errors-on-unmatched ${files.join(" ")}`];
   },
   "*.y(a?)ml": ["yamllint -c .yamllint.yml"],
-  // "*.{scss,css}": ["stylelint --fix", "prettier --write"],
-  // "*.{png,jpeg,jpg,gif,svg}": [
-  //   "imagemin-lint-staged" // @davidsneighbour/imagemin-lint-staged
-  // ],
-  // "!(CHANGELOG)**/*.{md,markdown}": [
-  //   "markdownlint-cli2",
-  //   "npm run lint:vale"
-  // ],
-  // "**/*.ts?(x)": () => [
-  //   "tsc -p tsconfig.json --noEmit", "prettier --write"
-  // ],
-  // "layouts/**/*.*": [
-  //   "./bin/hugo/refactor layouts"
-  // ],
-  // "**/*.*": [
-  //   "npx secretlint"
-  // ]
+  "*.{scss,css}": ["stylelint --fix", "prettier --write"],
+  "*.{png,jpeg,jpg,gif,svg}": [
+    "imagemin-lint-staged", // @davidsneighbour/imagemin-lint-staged
+  ],
+  "!(CHANGELOG)**/*.{md,markdown}": ["markdownlint-cli2", "npm run lint:vale"],
+  "**/*.ts?(x)": () => ["tsc -p tsconfig.json --noEmit", "prettier --write"],
+  "**/*.*": ["npx secretlint"],
   "*.jsonnet": ["jsonnetfmt -i *.jsonnet"],
 };
