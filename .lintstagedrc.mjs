94e9e64c062cf283cce1e60c65b248dd956729e2
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-04-27 14:52:59 +0700

    fix: check yaml and yml files with yamllint in lintstaged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,38 +2,36 @@
 
 /**
  * @filename: .lintstagedrc.mjs
- * @type {import('lint-staged').Configuration}
+ * @type {import("lint-staged").Configuration}
  */
 export default {
-  // '*.{json,jsonc}': ['biome check --staged'],
-  // '.github/workflows/**/*.y(a?)ml': [
-  //   'zizmor --no-exit-codes',
+  // "*.{json,jsonc}": ["biome check --staged"],
+  // ".github/workflows/**/*.y(a?)ml": [
+  //   "zizmor --no-exit-codes",
   // ],
-  'package-lock.json': [
-    'lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm',
+  "package-lock.json": [
+    "lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm",
   ],
-  '*.{ts,tsx,(m|c)js,jsx}': (/** @type {string[]} */ files) => {
-    return [`biome check --no-errors-on-unmatched ${files.join(' ')}`]
+  "*.{ts,tsx,(m|c)js,jsx}": (/** @type {string[]} */ files) => {
+    return [`biome check --no-errors-on-unmatched ${files.join(" ")}`];
   },
-  '*.yaml': ['yamllint -c .yamllint.yml'],
-  // '*.{scss,css}': ['stylelint --fix', "prettier --write"],
-  // '*.{png,jpeg,jpg,gif,svg}': [
-  //   'imagemin-lint-staged' // @davidsneighbour/imagemin-lint-staged
+  "*.y(a?)ml": ["yamllint -c .yamllint.yml"],
+  // "*.{scss,css}": ["stylelint --fix", "prettier --write"],
+  // "*.{png,jpeg,jpg,gif,svg}": [
+  //   "imagemin-lint-staged" // @davidsneighbour/imagemin-lint-staged
   // ],
-  // '!(CHANGELOG)**/*.{md,markdown}': [
-  //   'markdownlint-cli2',
-  //   'npm run lint:vale'
+  // "!(CHANGELOG)**/*.{md,markdown}": [
+  //   "markdownlint-cli2",
+  //   "npm run lint:vale"
   // ],
-  // '**/*.ts?(x)': () => [
-  //   'tsc -p tsconfig.json --noEmit', "prettier --write"
+  // "**/*.ts?(x)": () => [
+  //   "tsc -p tsconfig.json --noEmit", "prettier --write"
   // ],
-  // 'layouts/**/*.*': [
-  //   './bin/hugo/refactor layouts'
+  // "layouts/**/*.*": [
+  //   "./bin/hugo/refactor layouts"
   // ],
-  // '**/*.*': [
-  //   'npx secretlint'
+  // "**/*.*": [
+  //   "npx secretlint"
   // ]
-  '*.jsonnet': [
-    'jsonnetfmt -i *.jsonnet',
-  ],
-}
+  "*.jsonnet": ["jsonnetfmt -i *.jsonnet"],
+};
