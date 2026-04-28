278e17cb43451948453b70be39fe5d62c430baa0
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-04-29 05:19:42 +0700

    fix: work through deprecated and legacy functions

--- bashrc/lib/90-compat/dnb-legacy-aliases.bash
+++ bashrc/lib/90-compat/dnb-legacy-aliases.bash
@@ -1,25 +1,8 @@
 # shellcheck shell=bash
 
 # Compatibility aliases for older scripts. Remove once all callers migrated.
-
-# searchfor __dnb_check_requirements --path /home/patrick
-
-__dnb_check_requirements() { dnb_check_requirements "$@"; }
-__dnb_load_env() { dnb_load_env "$@"; }
-__dnb_log() {
-  if [[ "$#" -ge 2 ]]; then
-    dnb_log "${2}" "${1}"
-  else
-    dnb_log info "$*"
-  fi
-}
-__dnb_init_log() { dnb_log_init; }
-__dnb_error() { dnb_error "$@"; }
-__dnb_create_directory() { dnb_create_directory "$@"; }
-path_sanitise() { dnb_path_sanitise "$@"; }
-execute() {
-  printf 'DEPRECATED: execute used eval and was replaced by dnb_execute_lines.\n' >&2
-  return 1
-}
-package() { dnb_archive_create "$@"; }
-unpackage() { dnb_archive_extract "$@"; }
+# find usages:
+# `searchfor __dnb_check_requirements --path /path/to/check`
+#
+# aliasing via
+# __dnb_error() { dnb_error "$@"; }
--- bashrc/lib/REVIEW-SUMMARY.md
+++
@@ -1,7 +0,0 @@
-# Next Steps
-
-* go through 90-compat
-
-dotfiles on  main [$✘!?⇕] Node 24.14.0 took 1m15s
-
-➜ searchfor __dnb_create_repopath --path /home/patrick
