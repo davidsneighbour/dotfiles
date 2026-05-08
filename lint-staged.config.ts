ff6cb1bb74266c67d68d9088c0728b463a7a6d40
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-05-08 13:43:15 +0700

    ci(fix): update lint-staged configurations and config files

---
+++ lint-staged.config.ts
@@ -0,0 +1,23 @@
+/**
+ * @filename: .lintstagedrc.mjs
+ * @type {import("lint-staged").Configuration}
+ */
+export default {
+  '*.{json,jsonc}': ['biome check --write --staged'],
+  '.github/workflows/**/*.y(a?)ml': ['zizmor --no-exit-codes --fix'],
+  'package-lock.json': [
+    'lockfile-lint --path package-lock.json --validate-https --allowed-hosts npm',
+  ],
+  '*.{ts,tsx,(m|c)js,jsx}': [
+    'biome check --write --staged --no-errors-on-unmatched',
+  ],
+  '*.y(a?)ml': ['yamllint -c .yamllint.yml'],
+  '*.{scss,css}': ['stylelint --fix'],
+  '*.{png,jpeg,jpg,gif,svg}': [
+    'imagemin-lint-staged', // @davidsneighbour/imagemin-lint-staged
+  ],
+  '!(CHANGELOG)**/*.{md,markdown}': ['markdownlint-cli2'],
+  '**/*.ts?(x)': () => ['tsc-files --noEmit --pretty'],
+  '**/*.*': ['secretlint --no-glob'],
+  '*.jsonnet': ['jsonnetfmt -i *.jsonnet'],
+};
