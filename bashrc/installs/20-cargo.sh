a925b4b16ea33ffda9befb728dad30cbae7dc6e4
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-05-22 05:12:46 +0700

    fix(installs): update cargo/rust install and add eww to cargo packages

--- bashrc/installs/20-cargo.sh
+++ bashrc/installs/20-cargo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
--- bashrc/installs/90-cargo-packages.sh
+++ bashrc/installs/90-cargo-packages.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+sudo apt install --yes \
+  build-essential \
+  git \
+  pkg-config \
+  libgtk-3-dev \
+  libglib2.0-dev \
+  libpango1.0-dev \
+  libgdk-pixbuf-2.0-dev \
+  libcairo2-dev \
+  libdbusmenu-gtk3-dev \
+  libx11-dev \
+  libxrandr-dev \
+  libxi-dev \
+  libxext-dev \
+  libxcb1-dev \
+  libdbus-1-dev \
+  libgtk-layer-shell-dev
+
+clone https://github.com/elkowar/eww ~/github.com/elkowar/eww
+cd ~/github.com/elkowar/eww
+
+cargo clean
+cargo build --release --no-default-features --features x11
+
+mv target/release/eww ~/.local/bin/
+
+eww --help
