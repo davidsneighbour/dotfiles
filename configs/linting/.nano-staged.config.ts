4b7c37ac5caca50d95357ae10dfeed142c4e9dd6
Patrick Kollitsch <davidsneighbourdev+gh@gmail.com>
2026-05-30 08:34:24 +0700

    ci(fix): remove nano-staged from setup

--- configs/linting/.nano-staged.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Configuration } from 'nano-staged';
-
-export default {
-  '*': 'oxfmt --no-error-on-unmatched-pattern',
-  '**/*.{js,ts,jsx,tsx}': 'oxlint',
-  '**/*.css': 'stylelint --fix',
-} as Configuration;
